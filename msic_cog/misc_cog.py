f324daf514d0de1fc9c45370b8ee2cdaa4c65636
DavFount <47730893+DavFount@users.noreply.github.com>
2021-11-16 08:55:32 -0800

    Added Misc Commands Cog

---
+++ msic_cog/misc_cog.py
@@ -0,0 +1,28 @@
+import discord
+import re
+from discord.ext import tasks
+from redbot.core import Config, commands
+import random
+
+
+class MiscCog(commands.Cog):
+    """ Misc Commands for Clan ChX """
+
+    __version__ = '1.0.0'
+
+    def __init__(self, bot):
+        self.bot = bot
+
+        self.config = Config.get_conf(
+            self, identifier=56456541165166, force_registration=True
+        )
+
+        # self.config.register_guild(
+        #     log_channel=None)
+
+    @commands.command()
+    @commands.guild_only()
+    async def roll(self, ctx: commands.Context):
+        """ Roll between min and max to compete for items. """
+        num = random.randrange(0, 101)
+        ctx.reply(f'{ctx.author.mention} rolled {num}')
